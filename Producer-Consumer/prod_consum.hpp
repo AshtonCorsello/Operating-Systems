8febea063f32265e4e77c64b3b33ec11b799f07b
AshtonCorsello <103138954+AshtonCorsello@users.noreply.github.com>
2022-04-07 18:42:02 -0400

    Create prod_consum.hpp

---
+++ Producer-Consumer/prod_consum.hpp
@@ -0,0 +1,28 @@
+#ifndef PROD_CONSUM_H                                                                                      
+#define PROD_CONSUM_H                                                                                      
+                                                                                                      
+#include <sys/mman.h>                                                                                 
+#include <sys/stat.h>                                                                                 
+#include <sys/shm.h>                                                                                  
+#include <fcntl.h>                                                                                    
+#include <semaphore.h>                                                                                
+#include <stdio.h>                                                                                    
+#include <stdlib.h>                                                                                   
+#include <unistd.h>                                                                                   
+#include <iostream>                                                                                   
+                                                                                                      
+struct SharedMem {                                                                                    
+        int buffer[2];                                                                                
+        sem_t semFull;                                                                                
+        sem_t semEmpty;                                                                               
+        sem_t Mutex;                                                                                  
+};                                                                                                    
+                                                                                                      
+char *pathName;                                                                                       
+                                                                                                      
+void error(std::string msg) {                                                                         
+        std::cerr <<"ERROR: " << msg << " failed. Terminating." << std::endl;                         
+        exit(1);                                                                                      
+}                                                                                                     
+                                                                                                      
+#endif     
